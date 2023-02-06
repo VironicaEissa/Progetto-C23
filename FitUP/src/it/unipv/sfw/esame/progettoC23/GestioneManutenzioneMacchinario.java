93dde4459f341e193f3f9a1a20435d477bdba553
Rebecca Maccagni <119804491+RebeccaMaccagni@users.noreply.github.com>
2023-02-06 19:29:47 +0100

    Continuazione scheletro
    Co-Authored-By: DeniseVaccarella <119804550+DeniseVaccarella@users.noreply.github.com>
    Co-Authored-By: Stefany <stefanymaria.vecchio01@universitadipavia.it>

---
+++ FitUP/src/it/unipv/sfw/esame/progettoC23/GestioneManutenzioneMacchinario.java
@@ -0,0 +1,11 @@
+package it.unipv.sfw.esame.progettoC23;
+
+public class GestioneManutenzioneMacchinario {
+private Macchinario NomeMacchinario;
+
+public GestioneManutenzioneMacchinario(Macchinario nomeMacchinario) {
+		NomeMacchinario = nomeMacchinario;
+	}
+	   
+
+}
